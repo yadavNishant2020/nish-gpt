aea397e7abb273a93b1557769d1c3fe09a506f85
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-01-26 16:53:45 +0530

    Integrating with OpenAI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,24 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [input, setInput] = useState("");
+  const [chatLog, setchatLog] = useState([
+    {
+      user: "gpt",
+      message: "How can I help you today?",
+    },
+    {
+      user: "me",
+      message: "i want to use NishGPT today!!",
+    }
+  ]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setchatLog([...chatLog, { user: "me", message: `${input}` }]);
+    setInput("");
+  }
+
   return (
     <div className="App">
       <aside className="sidemenu">
@@ -10,33 +28,43 @@ function App() {
         </div>
       </aside>
       <section className="chatBox">
-         <div className="chatLog">
-          <div className="chatMessage ">
-            <div className="chat-message-center">
-            <div className="avatar">
-            
-            </div>
-            <div className="message">
-Hello
-            </div>
-            </div>
-
-          </div>
-          <div className="chatMessage chatGpt">
-            <div className="chat-message-center">
-            <div className="avatar">
-<img src="https://pbs.twimg.com/profile_images/1598924796372422656/nEcoIDXz_400x400.jpg" alt="" srcset="" />            </div>
-            <div className="message">
-I am AI!            </div>
-            </div>
-
-          </div>
-         </div>
+        <div className="chatLog">
+          {chatLog.map((message, index) => (
+            <ChatMessage key={index} message={message} />
+          ))}
+        </div>
         <div className="chatInput">
-          <input className="chatInputTextarea" placeholder="Type your question here" ></input>
-          </div></section>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="chatInputTextarea"
+              placeholder="Type your question here"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            ></input>
+          </form>
+        </div>
+      </section>
     </div>
   );
 }
+
+const ChatMessage = ({ message }) => {
+  return (
+    <div className={`chatMessage ${message.user == "gpt" && "chatGpt"}`}>
+      <div className="chat-message-center">
+        <div className={`avatar ${message.user == "gpt" && "chatGpt"}`}>
+          {message.user == "gpt" && (
+            <img
+              src="https://pbs.twimg.com/profile_images/1598924796372422656/nEcoIDXz_400x400.jpg"
+              alt=""
+              srcset=""
+            />
+          )}
+        </div>
+        <div className="message">{message.message}</div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 // 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
-const { Configuration, OpenAIApi }= require("openai");
+const { Configuration, OpenAIApi } = require("openai");
+const express = require('express')
+
 const configuration = new Configuration({
     organization: "org-RncNb0vy89sF0j37W3d4UnZ3",
-    apiKey: "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",
+    apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
-const response = await openai.createCompletion({
-    "model": "text-davinci-003",
-    "prompt": "Say this is a test",
-    "max_tokens": 7,
-    "temperature": 0,
-  });
-  
+
+// express api to call above function
+
+const app = express()
+const port = 3080
+
+app.post('/', async(req, res) => {
+    const response =  await openai.createCompletion({
+        "model": "text-davinci-003",
+        "prompt": "Say this is a test",
+        "max_tokens": 7,
+        "temperature": 0,
+      });
+      console.log(response.data.choices[0].text)
+      res.json({
+        data: response.data
+      })
+});
